2e6749c0fed722d3b464fa25adb7ebb33834cf10
Joao Pedro <blvck.joao37@gmail.com>
2026-02-22 13:06:26 -0300

    Update - PerceptionNode.hpp criado

--- src/maze_robot/include/maze_robot/perception_node.hpp
+++ src/maze_robot/include/maze_robot/perception_node.hpp
@@ -1,5 +1,30 @@
 #ifndef _PERCEPTION_NODE_HPP_
 #define _PERCEPTION_NODE_HPP_
 
+#include <rclcpp/rclcpp.hpp>
+#include <sensor_msgs/msg/image.hpp>
+#include <std_msgs/msg/int32.hpp>
+#include <cv_bridge/cv_bridge.hpp>
+#include <opencv2/opencv.hpp>
+
+
+class PerceptionNode : public rclcpp::Node {
+        public:
+                PerceptionNode();
+
+        private:
+                void image_callback(const sensor_msgs::msg::Image::SharedPtr msg);
+
+                rclcpp::Subscription<sensor_msgs::msg::Image>::SharedPtr image_sub_;
+
+                rclcpp::Publisher<std_msgs::msg:Int32>::SharedPtr red_pub_;
+                rclcpp::Publisher<std_msgs::msg:Int32>::SharedPtr blue_pub_;
+                rclcpp::Publisher<std_msgs::msg:Int32>::SharedPtr green_pub_;
+
+                int red_counter_;
+                int blue_counter_;
+                int green_counter_;
+};
+
 
 #endif
